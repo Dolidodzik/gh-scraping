57a1059a4a91a43050ccafbd2d55cddf497833e5
lesiu <teethtard321@gmail.com>
2023-06-29 08:28:22 +0200

    emails from patch

--- gh_scraping/urls.py
+++ gh_scraping/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import main.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dump_csv/', views.dump_csv, name="dump_csv")
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import csv
+from .models import Contributor
 
-# Create your views here.
+
+def dump_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="users.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['created_at', 'updated_at', 'github_login', 'github_created_at', 'name', 'email', 'email_from_patch', 'followers_count', 'hireable', 'twitter_username', 'avatar_url', 'blog', 'bio', 'company', 'location'])
+
+    contributors = Contributor.objects.all().values_list('created_at', 'updated_at', 'github_login', 'github_created_at', 'name', 'email', 'email_from_patch', 'followers_count', 'hireable', 'twitter_username', 'avatar_url', 'blog', 'bio', 'company', 'location')
+    for contributor in contributors:
+        writer.writerow(contributor)
+
+    return response
+    
